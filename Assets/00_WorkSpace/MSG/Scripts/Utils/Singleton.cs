3cb9b4a00baac11d1c05610c5d02ca14ac7d288a
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-18 01:49:30 +0900

    feat: 싱글톤 유틸 클래스 추가

---
+++ Assets/00_WorkSpace/MSG/Scripts/Utils/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSG
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        GameObject singletonObj = new GameObject(typeof(T).Name);
+                        _instance = singletonObj.AddComponent<T>();
+                        DontDestroyOnLoad(singletonObj);
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        protected void SingletonInit()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(_instance);
+            }
+        }
+    }
+}
