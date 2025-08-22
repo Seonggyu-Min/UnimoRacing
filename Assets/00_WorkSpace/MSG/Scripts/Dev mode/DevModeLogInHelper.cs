b2814da5bb608ae3525c50a0e3009dd34aa45bcc
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-22 17:38:24 +0900

    feat: 개발용 자동 로그인 및 회원가입 기능 추가

--- Assets/00_WorkSpace/MSG/Scripts/Dev mode/DevModeLogInHelper.cs
+++ Assets/00_WorkSpace/MSG/Scripts/Dev mode/DevModeLogInHelper.cs
@@ -1,7 +1,15 @@
-﻿using Firebase;
+﻿#if UNITY_EDITOR
+#define UNITY_DEV_MODE
+#endif
+
+#if UNITY_DEV_MODE
 using Firebase.Auth;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 
@@ -22,7 +30,7 @@ namespace MSG
             _notDevUIs.SetActive(false);
         }
 
-        public void OnClickLogInButton()
+        public void OnClickLogInOrCreateButton()
         {
             var email = _emailInputField ? _emailInputField.text.Trim() : "";
             var password = _passwordInputField ? _passwordInputField.text : "";
@@ -34,10 +42,51 @@ namespace MSG
             }
 
             Debug.Log("[DevLogin] 이메일 로그인 시도…");
+            SignInOrCreate(email, password).Forget();
+        }
 
-            var auth = FirebaseAuth.DefaultInstance;
-            // 아직 미구현
+        private async Task SignInOrCreate(string email, string password)
+        {
+            Debug.Log("[DevLogin] SignInOrCreate 시작");
+            try
+            {
+                var auth = FirebaseManager.Instance.Auth;
+
+                // 로그인 시도
+                AuthResult signInResult = await auth.SignInWithEmailAndPasswordAsync(email, password);
+                FirebaseUser user = signInResult.User;
+
+                // 성공 처리
+                OnDevSignedIn(user);
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    var auth = FirebaseManager.Instance.Auth;
+
+                    // 없으면 가입 및 로그인
+                    AuthResult createResult = await auth.CreateUserWithEmailAndPasswordAsync(email, password);
+                    FirebaseUser user = createResult.User;
+
+                    OnDevSignedIn(user);
+                }
+                catch (Exception createEx)
+                {
+                    Debug.LogWarning($"[DevLogin] 실패: {createEx.Message}");
+                }
+            }
+        }
+
+        private void OnDevSignedIn(FirebaseUser user)
+        {
+            Debug.Log($"[DevLogin] 파이어베이스 로그인 상태: 이메일: {user.Email} UID: {user.UserId}");
+
+            PhotonNetwork.AuthValues = new AuthenticationValues(user.UserId);
+
+            var flow = FindAnyObjectByType<AuthFlowController>();
+            flow?.SendMessage("HandleSignInSucceeded", user, SendMessageOptions.DontRequireReceiver);
         }
     }
-
 }
+#endif
