b0982665985bbf8de5e3692707c7314b9006988f
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-25 18:04:33 +0900

    feat: 클라이언트 상태 디버깅용 클래스 추가

---
+++ Assets/00_WorkSpace/MSG/Scripts/Debugger/ClientStateDebugger.cs
@@ -0,0 +1,46 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class ClientStateDebugger : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _debugText;
+
+
+        private void Update()
+        {
+            var state = PhotonNetwork.NetworkClientState;
+            var inRoom = PhotonNetwork.InRoom;
+
+            if (inRoom && PhotonNetwork.CurrentRoom != null)
+            {
+                var room = PhotonNetwork.CurrentRoom;
+                // UserId가 null일 수 있으니 fallback 준비
+                var ids = PhotonNetwork.PlayerList
+                    .Select(p => !string.IsNullOrEmpty(p.UserId) ? p.UserId
+                            : (!string.IsNullOrEmpty(p.NickName) ? p.NickName
+                            : $"Actor#{p.ActorNumber}"));
+
+                _debugText.text =
+                    $"ClientState : {state}\n" +
+                    $"InRoom      : {inRoom}\n" +
+                    $"Room        : {room.Name} ({room.PlayerCount}/{room.MaxPlayers})  Open={room.IsOpen} Visible={room.IsVisible}\n" +
+                    $"Players     : {string.Join(", ", ids)}";
+            }
+            else
+            {
+                _debugText.text =
+                    $"ClientState : {state}\n" +
+                    $"InRoom      : {inRoom}\n" +
+                    $"Room        : (Not in room)\n" +
+                    $"Players     : -";
+            }
+        }
+    }
+}
