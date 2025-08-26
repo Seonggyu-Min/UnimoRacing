e7da80b5ce7ed9e5d31142ee1d88c92813de149c
DevIRU <deviru0413@gmail.com>
2025-08-26 16:21:59 +0900

    [feat]: ConfigSO들 구현 및 추가
    - InGameRaceRulesConfig(InGameManager에서 사용)
    - RoomRaceGameConfig(RoomManager에서 사용)

---
+++ Assets/00_WorkSpace/YSJ/SO/RoomRaceGameConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "RaceGameConfig", menuName = "Game/RaceGameConfig")]
+public sealed class RoomRaceGameConfig : ScriptableObject
+{
+    [Min(2)] public int RoomRacePlayer          = 4;    // 방 최대 인원
+    [Min(1)] public int RaceChoosableMapCount   = 3;    // 레이싱 선택 가능한 맵 수
+
+    public static RoomRaceGameConfig Load()
+        => Resources.Load<RoomRaceGameConfig>("Config/RaceGameConfig");
+}
