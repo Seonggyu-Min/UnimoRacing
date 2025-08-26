fe6673064c2adbdefff34617a773beed5e46dc44
baechu8716 <baechu8716@naver.com>
2025-08-26 14:33:59 +0900

    [chore]

--- Assets/00_WorkSpace/YTW/Manager/Manager/Manager.cs
+++ Assets/00_WorkSpace/YTW/Manager/Manager/Manager.cs
@@ -3,38 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using YTW;
 
-public class Manager : Singleton<Manager>
+namespace YTW
 {
-    public static AudioManager Audio { get; private set; }
-    public static SceneManager Scene { get; private set; }
-    public static ResourceManager Resource { get; private set; }
-
-    protected override void Awake()
+    public class Manager : Singleton<Manager>
     {
-        base.Awake();
+        public static AudioManager Audio { get; private set; }
+        public static SceneManager Scene { get; private set; }
+        public static ResourceManager Resource { get; private set; }
 
-        Audio = AudioManager.Instance;
-        Scene = SceneManager.Instance;
-        Resource = ResourceManager.Instance;
+        protected override void Awake()
+        {
+            base.Awake();
+
+            Audio = AudioManager.Instance;
+            Scene = SceneManager.Instance;
+            Resource = ResourceManager.Instance;
+        }
+
+        // UI 이벤트를 위한 연결 함수
+        public void SetMasterVolume(float value)
+        {
+            // UI 슬라이더로부터 받은 값을 AudioManager의 실제 함수로 전달
+            if (Audio != null)
+                Audio.SetMasterVolume(value);
+        }
+
+        public void SetBgmVolume(float value)
+        {
+            if (Audio != null)
+                Audio.SetBGMVolume(value);
+        }
+
+        public void SetSfxVolume(float value)
+        {
+            if (Audio != null)
+                Audio.SetSFXVolume(value);
+        }
     }
 
-    // UI 이벤트를 위한 연결 함수
-    public void SetMasterVolume(float value)
-    {
-        // UI 슬라이더로부터 받은 값을 AudioManager의 실제 함수로 전달
-        if (Audio != null)
-            Audio.SetMasterVolume(value);
-    }
-
-    public void SetBgmVolume(float value)
-    {
-        if (Audio != null)
-            Audio.SetBGMVolume(value);
-    }
-
-    public void SetSfxVolume(float value)
-    {
-        if (Audio != null)
-            Audio.SetSFXVolume(value);
-    }
 }
