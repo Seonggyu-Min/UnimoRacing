0671f7dddf1185c2f0ce3146e58da6b6eb677d6f
ro9ro9 <101381135+ro9ro9@users.noreply.github.com>
2025-08-26 10:53:53 +0900

    [feat]옵션창,마이룸,친구창,상점 추가

--- Assets/00_WorkSpace/NTJ/FriendParty/PartyManager.cs
+++ Assets/00_WorkSpace/NTJ/FriendParty/PartyManager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class PartyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_Text partyCountText;  // "n/4"
+    [SerializeField] private Transform partySlotParent; // 파티 슬롯 부모
+    [SerializeField] private GameObject partySlotPrefab; // 파티 슬롯 프리팹
+
+    private List<string> currentParty = new List<string>();
+    private const int MaxParty = 4;
+       
+    // 파티 초대
+    public void InviteToParty(string friendUid, string friendName, int friendLevel, Sprite carIcon)
     {
-        
+        if (currentParty.Count >= MaxParty)
+        {
+            Debug.Log("파티가 꽉 찼습니다!");
+            return;
+        }
+
+        if (currentParty.Contains(friendUid))
+        {
+            Debug.Log($"{friendName}은(는) 이미 파티에 있습니다.");
+            return;
+        }
+
+        currentParty.Add(friendUid);
+
+        GameObject slot = Instantiate(partySlotPrefab, partySlotParent);
+        PartySlotUI slotUI = slot.GetComponent<PartySlotUI>();
+        slotUI.Setup(friendUid, carIcon, friendName, friendLevel); // uid 파라미터 추가
+
+        UpdatePartyCount();
     }
 
-    // Update is called once per frame
-    void Update()
+    // 파티 인원 카운트 업데이트
+    private void UpdatePartyCount()
     {
-        
+        partyCountText.text = $"{currentParty.Count}/{MaxParty}";
+    }
+
+    // 파티에서 제거
+    public void RemoveFromParty(string friendUid)
+    {
+        var slot = partySlotParent.GetComponentsInChildren<PartySlotUI>()
+                                  .FirstOrDefault(s => s.friendUid == friendUid);
+        if (slot != null)
+        {
+            Destroy(slot.gameObject);
+            currentParty.Remove(friendUid);
+            UpdatePartyCount();
+        }
     }
 }
