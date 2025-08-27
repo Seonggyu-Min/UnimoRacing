bad0131a24e3281f537a84ea7858ef4d1d050bcf
DevIRU <deviru0413@gmail.com>
2025-08-27 17:58:46 +0900

    [test]: 동기화 확인해주는 기능 추가

---
+++ Assets/00_WorkSpace/YSJ/PhotonKartLoadedChecker.cs
@@ -0,0 +1,16 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PhotonKartLoadedChecker : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        PlayerManager.Instance.SetRaceLoadedSelection();
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
