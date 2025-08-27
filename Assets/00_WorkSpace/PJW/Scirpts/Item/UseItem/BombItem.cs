1e6e0560b663a945fde34e8a1a8d65996a712efd
jongwon1205 <haegol5009@gmail.com>
2025-08-27 16:16:45 +0900

    [Fix] BombItem
    1. 돌리트랙 위에 스폰되도록 설정
    2. 폭탄에 맞으면 영구 정지되던 현상 수정
    3. 폭탄이 플레이어랑 영거리로 스폰되던 현상 수정

--- Assets/00_WorkSpace/PJW/Scirpts/Item/UseItem/BombItem.cs
+++ Assets/00_WorkSpace/PJW/Scirpts/Item/UseItem/BombItem.cs
@@ -1,21 +1,58 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace PJW
 {
-    
     public class BombItem : MonoBehaviour, IUsableItem
     {
         [SerializeField] private GameObject bombPrefab;
-        [SerializeField] private float distanceAhead = 5f;
+        [SerializeField] private float distanceAhead;
 
         public void Use(GameObject owner)
         {
-            if (bombPrefab == null || owner == null) return;
+            if (bombPrefab == null || owner == null)
+            {
+                Destroy(gameObject);
+                return;
+            }
 
-            Vector3 spawnPos = owner.transform.position + owner.transform.forward * distanceAhead;
-            Instantiate(bombPrefab, spawnPos, Quaternion.identity);
+            var cart = owner.GetComponentInParent<CinemachineDollyCart>();
+            var path = cart != null ? cart.m_Path : null;
+            if (cart == null || path == null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            var units = cart.m_PositionUnits;
+            float currentT = cart.m_Position;
+            float targetT = currentT + distanceAhead;
+
+            if(units == CinemachinePathBase.PositionUnits.Normalized)
+            {
+                if (path.Looped)
+                    targetT = Mathf.Repeat(targetT, 1f);
+                else
+                    targetT = Mathf.Clamp01(targetT);
+            }
+            else 
+            {
+                float pathLen = path.PathLength;
+                if (path.Looped)
+                    targetT = Mathf.Repeat(targetT, pathLen);
+                else
+                    targetT = Mathf.Clamp(targetT, 0f, pathLen);
+            }
+
+            // 트랙 위 위치와 회전
+            Vector3 spawnPos = path.EvaluatePositionAtUnit(targetT, units);
+            Quaternion spawnRot = path.EvaluateOrientationAtUnit(targetT, units);
+
+            Instantiate(bombPrefab, spawnPos, spawnRot);
+
+            Destroy(gameObject);
         }
     }
 }
