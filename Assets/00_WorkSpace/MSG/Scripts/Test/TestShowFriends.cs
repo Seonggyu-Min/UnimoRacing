1a8a54921606d4e80452854f0000fa67008be3a5
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-29 01:25:08 +0900

    feat: 테스트용 친구 버튼 생성 로직 추가
    - 기존의 매칭 시스템을 참조하여 일단 주석처리함

---
+++ Assets/00_WorkSpace/MSG/Scripts/Test/TestShowFriends.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class TestShowFriends : MonoBehaviour
+    {
+        //[SerializeField] private TestInviteButtonBehaviour prefab;
+
+        //private string CurrentUid => FirebaseManager.Instance.Auth.CurrentUser.UserId;
+
+        //private void Start()
+        //{
+        //    CreateFriendInviteButton();
+        //}
+
+        //private void CreateFriendInviteButton()
+        //{
+        //    DatabaseManager.Instance.GetOnMain(DBRoutes.FriendListRoot(CurrentUid), snap =>
+        //    {
+        //        if (!snap.Exists || !snap.HasChildren)
+        //        {
+        //            Debug.Log("친구 없음");
+        //            return;
+        //        }
+
+        //        var friendUids = new List<string>();
+        //        foreach (var c in snap.Children) friendUids.Add(c.Key);
+
+        //        int left = friendUids.Count;
+        //        foreach (var uid in friendUids)
+        //        {
+        //            DatabaseManager.Instance.GetOnMain(DBRoutes.Nickname(uid), s2 =>
+        //            {
+        //                string nick = s2.Value?.ToString() ?? uid;
+
+        //                var item = Instantiate(prefab, transform);
+        //                item.Init(uid, nick);
+        //            },
+        //            err =>
+        //            {
+        //                // 닉네임 실패 시 uid로 표시
+        //                var item = Instantiate(prefab, transform);
+        //                item.Init(uid, uid);
+        //            });
+        //        }
+        //    });
+        //}
+    }
+}
