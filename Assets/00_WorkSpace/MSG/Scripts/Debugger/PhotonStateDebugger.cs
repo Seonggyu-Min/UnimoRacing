9252c0505a6c5b0386fd7773e97fc709f9862a4d
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-29 01:23:41 +0900

    feat: 포톤 네트워크 상태 디버깅용 클래스 추가
    - Update에서 호출하여 성능은 좋지 않음

---
+++ Assets/00_WorkSpace/MSG/Scripts/Debugger/PhotonStateDebugger.cs
@@ -0,0 +1,53 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class PhotonStateDebugger : MonoBehaviour
+{
+    [SerializeField] private bool _logToConsole = true;
+    [SerializeField] private TMP_Text _uiText;
+
+    private void Update()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine($"[State] {PhotonNetwork.NetworkClientState}");
+
+        if (PhotonNetwork.InLobby)
+        {
+            sb.AppendLine("현재: 로비 안에 있음");
+        }
+        if (PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom != null)
+        {
+            Room room = PhotonNetwork.CurrentRoom;
+            sb.AppendLine($"현재: 룸 안에 있음");
+            sb.AppendLine($"룸 이름: {room.Name}");
+            sb.AppendLine($"룸 인원: {room.PlayerCount}/{room.MaxPlayers}");
+
+            foreach (var kvp in room.Players)
+            {
+                Player p = kvp.Value;
+                sb.AppendLine($"- Player {p.ActorNumber} | UserId={p.UserId} | Nick={p.NickName}");
+            }
+        }
+        else
+        {
+            sb.AppendLine("룸 안에 있지 않음");
+        }
+
+        string msg = sb.ToString();
+
+        if (_logToConsole)
+        {
+            Debug.Log(msg);
+        }
+        if (_uiText != null)
+        {
+            _uiText.text = msg;
+        }
+    }
+}
