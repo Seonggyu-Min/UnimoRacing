d334d68145d7f1ba70729e1dcb3085551c87b9d1
Seonggyu-Min <minsunggu705@gmail.com>
2025-08-29 01:52:06 +0900

    feat: 방 생성 네이밍 제공 헬퍼 클래스 추가

---
+++ Assets/00_WorkSpace/MSG/Scripts/MatchMaking/RoomMakeHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public static class RoomMakeHelper
+    {
+        public const int MAX_PLAYERS = 2;
+
+        public static string Personal(string uid)
+        {
+            return $"h_{uid}";
+        }
+
+        public static string PartyHome(string leaderUid)
+        {
+            return $"p_{leaderUid}";
+        }
+
+        public static string MatchCandidate()
+        {
+            return $"m_{DateTime.UtcNow:yyyyMMddHHmm}_{UnityEngine.Random.Range(1000, 9999)}";
+        }
+    }
+}
