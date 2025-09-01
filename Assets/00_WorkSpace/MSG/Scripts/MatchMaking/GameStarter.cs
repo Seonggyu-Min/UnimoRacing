3e85646a744441bad5d9731db137170d152d4bb5
Seonggyu-Min <minsunggu705@gmail.com>
2025-09-01 16:43:12 +0900

    [refactor]: 사용하지 않는 필드 삭제 및 디버그 로그 추가

--- Assets/00_WorkSpace/MSG/Scripts/MatchMaking/GameStarter.cs
+++ Assets/00_WorkSpace/MSG/Scripts/MatchMaking/GameStarter.cs
@@ -11,12 +11,13 @@ namespace MSG
     // TODO: 모든 방에 사람이 찼을 때 띄울 투표 로직 추가
     public class GameStarter : MonoBehaviourPunCallbacks
     {
-        [Header("Game Start")]
-        [SerializeField] private bool _startWhenRoomIsFull = true;   // 방이 가득 찼을 때만 시작
-        [SerializeField] private bool _startOnlyInMatchRoom = true;  // 매치룸에서만 시작 (홈룸 말고)
-
-        [Header("Refs")]
         [SerializeField] private PartyService _party;
+
+        [SerializeField] private bool _dontTryStartWhenInHomeRoom = true;  // 매치룸에서만 시작 (홈룸 말고)
+
+        private bool _isStarted = false; // TODO: false로 다시 바꾸는 로직도 필요
+
+        public bool IsStarted => _isStarted;
 
 
         public override void OnJoinedRoom()
@@ -31,20 +32,27 @@ namespace MSG
 
         private void TryStartGame()
         {
+            if (!PhotonNetwork.IsMasterClient) return;
+
             if (!PhotonNetwork.InRoom || PhotonNetwork.CurrentRoom == null) return;
 
             string roomName = PhotonNetwork.CurrentRoom.Name ?? string.Empty;
 
-            // 홈룸에서 시작하지 않도록 방어
-            if (_startOnlyInMatchRoom && !roomName.StartsWith("m_"))
+            // 홈룸에서 시작하지 않도록 방어, 파티룸에서는 인원이 다 차면 시도할 수 있음
+            if (_dontTryStartWhenInHomeRoom && roomName.StartsWith("h_")) // 프로퍼티로 바꾸는 것이 좋을 듯
             {
-                Debug.Log("홈룸이 아닌데 시작 시도되어 return");
+                Debug.Log("홈룸인데 시작 시도되어 return");
                 return;
             }
 
             // 방이 가득 찼는지 확인
             if (PhotonNetwork.CurrentRoom.PlayerCount >= PhotonNetwork.CurrentRoom.MaxPlayers)
             {
+                Debug.Log($"PlayerCount: {PhotonNetwork.CurrentRoom.PlayerCount}, MaxPlayers: {PhotonNetwork.CurrentRoom.MaxPlayers}");
+                foreach (var p in PhotonNetwork.CurrentRoom.Players)
+                {
+                    Debug.Log($"방에 있는 인원의 uid: {p.Value.UserId}");
+                }
                 StartGame();
                 return;
             }
@@ -58,6 +66,15 @@ namespace MSG
                 PhotonNetwork.CurrentRoom.IsVisible = false;
             }
 
+            _isStarted = true;
+
+            StartCoroutine(VoteRoutine());
+        }
+
+        private IEnumerator VoteRoutine() // TODO: 투표 로직 추가
+        {
+            Debug.Log("투표 시작");
+            yield return new WaitForSeconds(5f);
             PhotonNetwork.LoadLevel(2); // 임의로 2
         }
     }
