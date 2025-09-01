c1b74f5339ae23058231d93edfa262655f9e3f73
Seonggyu-Min <minsunggu705@gmail.com>
2025-09-01 16:42:05 +0900

    [feat]: 파티 관련 DM 기능 추가

--- Assets/00_WorkSpace/MSG/Scripts/MatchMaking/ChatDM.cs
+++ Assets/00_WorkSpace/MSG/Scripts/MatchMaking/ChatDM.cs
@@ -8,12 +8,29 @@ using UnityEngine;
 namespace MSG
 {
     public enum DMType
-    { 
-        Invite, // 파티 초대
+    {
+        Invite, // 방으로 들어오라는 명령
         Out,    // 리더가 파티원에게 즉시 방 나가기 발신
-        Recall,
-        Cancel
+        Recall, // 파티룸으로 복귀 명령, Out을 먼저 보내고 방이 생성된 뒤에 호출됨
+        Cancel, // 매칭 취소
+
+        PartyInvite,  // 파티에 초대
+        PartyAccept,  // 파티 초대에 승낙
+        PartyReject,  // 파티 초대에 거절
+        PartySync,    // 파티원들 동기화
+
+        PartyLeave,   // 자신이 나가기
+        //PartyKick,  // 미구현 상태, 파티원 강퇴
+        PartyDisband  // 파티 해산 -> 리더가 나가면 파티 해산으로
     }
+
+    [Serializable] public class PartyInviteMsg { public string partyId; public string leaderUid; public string[] members; }
+    [Serializable] public class PartyAcceptMsg { public string partyId; public string accepterUid; }
+    [Serializable] public class PartyRejectMsg { public string partyId; public string rejecterUid; public string reason; }
+    [Serializable] public class PartySyncMsg { public string partyId; public string leaderUid; public string[] members; }
+    [Serializable] public class PartyLeaveMsg { public string leaderUid; public string leaverUid; }
+    [Serializable] public class PartyDisbandMsg { public string leaderUid; }
+
 
     public class ChatDM : MonoBehaviour, IChatClientListener
     {
@@ -44,10 +61,41 @@ namespace MSG
             EnsureClient();
             TryConnect();
         }
+
         public void SendInvite(string uid, string roomName) { Send(uid, Build("INVITE", roomName)); }
         public void SendOut(string uid) { Send(uid, Build("OUT", string.Empty)); }
         public void SendRecall(string uid, string homeRoom) { Send(uid, Build("RECALL", homeRoom)); }
         public void SendCancel(string uid) { Send(uid, Build("CANCEL", string.Empty)); }
+        public void SendPartyInvite(string toUid, string partyId, string leaderUid, string[] members)
+        {
+            var msg = new PartyInviteMsg { partyId = partyId, leaderUid = leaderUid, members = members };
+            Send(toUid, BuildJson("PARTY_INVITE", msg));
+        }
+        public void SendPartyAccept(string toUid, string partyId, string accepterUid)
+        {
+            var msg = new PartyAcceptMsg { partyId = partyId, accepterUid = accepterUid };
+            Send(toUid, BuildJson("PARTY_ACCEPT", msg));
+        }
+        public void SendPartyReject(string toUid, string partyId, string rejecterUid, string reason = "")
+        {
+            var msg = new PartyRejectMsg { partyId = partyId, rejecterUid = rejecterUid, reason = reason ?? "" };
+            Send(toUid, BuildJson("PARTY_REJECT", msg));
+        }
+        public void SendPartySync(string toUid, string partyId, string leaderUid, string[] members)
+        {
+            var msg = new PartySyncMsg { partyId = partyId, leaderUid = leaderUid, members = members };
+            Send(toUid, BuildJson("PARTY_SYNC", msg));
+        }
+        public void SendPartyLeave(string leaderUid, string leaverUid)
+        {
+            var msg = new PartyLeaveMsg { leaderUid = leaderUid, leaverUid = leaverUid };
+            Send(leaderUid, BuildJson("PARTY_LEAVE", msg));
+        }
+        public void SendPartyDisband(string toUid, string leaderUid)
+        {
+            var msg = new PartyDisbandMsg { leaderUid = leaderUid };
+            Send(toUid, BuildJson("PARTY_DISBAND", msg));
+        }
 
         #endregion
 
@@ -103,6 +151,11 @@ namespace MSG
             if (string.IsNullOrEmpty(body)) return head;
             return string.Concat(head, ":", body);
         }
+        private string BuildJson(string head, object body)
+        {
+            string json = JsonUtility.ToJson(body);
+            return string.Concat(head, ":", json);
+        }
 
         private void Send(string toUid, string content)
         {
@@ -150,14 +203,18 @@ namespace MSG
 
         public void OnPrivateMessage(string sender, object message, string channelName)
         {
+            if (sender == _selfUid)
+            {
+                Debug.Log("자기 자신에게 보낸 DM이라 return");
+                return;
+            }
+
             string raw = message as string;
             if (string.IsNullOrEmpty(raw)) return;
-
 
             int idx = raw.IndexOf(':');
             string head = idx >= 0 ? raw.Substring(0, idx) : raw;
             string body = idx >= 0 ? raw.Substring(idx + 1) : string.Empty;
-
 
             DMType t = DMType.Cancel;
             if (head == "INVITE") t = DMType.Invite;
@@ -165,9 +222,19 @@ namespace MSG
             else if (head == "RECALL") t = DMType.Recall;
             else if (head == "CANCEL") t = DMType.Cancel;
 
+            else if (head == "PARTY_INVITE") t = DMType.PartyInvite;
+            else if (head == "PARTY_ACCEPT") t = DMType.PartyAccept;
+            else if (head == "PARTY_REJECT") t = DMType.PartyReject;
+            else if (head == "PARTY_SYNC") t = DMType.PartySync;
+
+            else if (head == "PARTY_LEAVE") t = DMType.PartyLeave;
+            else if (head == "PARTY_DISBAND") t = DMType.PartyDisband;
 
             Action<string, DMType, string> handler = OnDirectMessageReceived;
-            if (handler != null) handler(sender, t, body);
+            if (handler != null)
+            {
+                handler(sender, t, body);
+            }
         }
 
         #endregion
