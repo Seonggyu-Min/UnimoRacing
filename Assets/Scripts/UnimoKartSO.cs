f86460ca3e56c1a9999c1453e5119a77f988af38
DevIRU <deviru0413@gmail.com>
2025-09-05 17:35:03 +0900

    [feat]: 캐릭터 및 카트 스크립터블 오브젝트 추가 생성

---
+++ Assets/Scripts/UnimoKartSO.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewUnimoKartSO", menuName = "Unimo/Kart")]
+public class UnimoKartSO : ScriptableObject
+{
+    [Header("기본 정보")]
+    [Tooltip("차량 ID")]
+    public int carId;
+
+    [Tooltip("차량 이름")]
+    public string carName;
+
+    [Tooltip("차량 프리팹")]
+    public GameObject kartPrefab;
+
+    [TextArea]
+    [Tooltip("차량 설명")]
+    public string carDesc;
+
+    [Header("스킬")]
+    [Tooltip("패시브 스킬 ID")]
+    public int passiveSkillId;
+}
