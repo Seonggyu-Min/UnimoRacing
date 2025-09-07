b6660afc88b4eef0ee7df0e0d1312ef8fe353732
Seonggyu-Min <minsunggu705@gmail.com>
2025-09-08 00:32:00 +0900

    feat: 접속 상태 활성화 기능 추가

---
+++ Assets/00_WorkSpace/MSG/Scripts/Friends/PresenceService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class PresenceService : MonoBehaviour
+    {
+        [SerializeField] private PresenceLogic _presenceLogic;
+        [SerializeField] private bool _isInGame;
+
+        private void Start()
+        {
+            _presenceLogic.SetOnlineStatus(true);
+            _presenceLogic.SetInGameStatus(_isInGame);
+        }
+    }
+}
