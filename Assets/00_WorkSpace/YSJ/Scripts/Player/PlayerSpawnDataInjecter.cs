b7e116c5121ee8733e2e650c0d7d309009930cab
DevIRU <deviru0413@gmail.com>
2025-09-08 11:20:33 +0900

    [feat]: 플레이어 데이터 인젝터 기능 추가

---
+++ Assets/00_WorkSpace/YSJ/Scripts/Player/PlayerSpawnDataInjecter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using YSJ.Util;
+
+public class PlayerSpawnDataInjecter : MonoBehaviour
+{
+    [Header("Setup Config")]
+    [SerializeField] private    bool                    _isRandomUse = false; // 랜덤 사용 여부
+    [SerializeField] private    int                     _useIndex = 0;
+
+    // data
+    [Header("Setup Data")]
+    [SerializeField] private    List<PlayerSpawnData>   _playerSpawnDatas;
+
+    public PlayerSpawnData GetData()
+    {
+        if (_playerSpawnDatas == null || _playerSpawnDatas.Count == 0)
+        {
+            this.PrintLog("인젝터 쪽, 리스트에 문제 발생", LogType.Error);
+        }
+
+        if (_isRandomUse)
+            return _playerSpawnDatas[Random.Range(0, _playerSpawnDatas.Count)];
+
+        return _playerSpawnDatas[_useIndex];
+    }
+}
