e9645370b59aff3005acab925f888984395e626e
Seonggyu-Min <minsunggu705@gmail.com>
2025-09-10 16:51:09 +0900

    feat: 씬에 맞는 BGM을 자동으로 재생하는 로직 추가

---
+++ Assets/00_WorkSpace/MSG/Scripts/BGMPlayer/BgmAutoPlayer.cs
@@ -0,0 +1,80 @@
+﻿using EditorAttributes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using YTW;
+using SceneManager = UnityEngine.SceneManagement.SceneManager;
+
+
+namespace MSG
+{
+    [Serializable]
+    public class SceneBGMBinder
+    {
+        [SerializeField, SceneDropdown] private string _scene;
+        //[SerializeField] private List<string> _bgmList;
+        [SerializeField] private string _bgm;
+
+        public string Scene => _scene;
+        public string BGM => _bgm;
+    }
+
+    // 인게임 씬은 적용하지 않습니다.
+    public class BgmAutoPlayer : MonoBehaviour
+    {
+        [SerializeField] private List<SceneBGMBinder> _sceneBgmList;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        private void OnEnable()
+        {
+            SceneManager.activeSceneChanged += OnActiveSceneChanged;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(StartAfterAudioReady());
+        }
+
+        private void OnDisable()
+        {
+            SceneManager.activeSceneChanged -= OnActiveSceneChanged;
+        }
+
+
+        private void OnActiveSceneChanged(Scene prev, Scene next)
+        {
+            ApplyBgmFor(next);
+        }
+
+        private void ApplyBgmFor(Scene scene)
+        {
+            Manager.Audio.StopBGM();
+
+            for (int i = 0; i < _sceneBgmList.Count; i++)
+            {
+                var bind = _sceneBgmList[i];
+                if (scene.name == bind.Scene)
+                {
+                    Manager.Audio.PlayBGM(bind.BGM);
+                    return;
+                }
+            }
+
+            Debug.LogWarning($"[BgmAutoPlayer] {scene.name} 씬에 BGM이 등록되지 않았습니다.");
+        }
+
+        private IEnumerator StartAfterAudioReady()
+        {
+            if (!Manager.Audio.IsInitialized)
+                yield return new WaitUntil(() => Manager.Audio.IsInitialized);
+
+            ApplyBgmFor(SceneManager.GetActiveScene());
+        }
+    }
+}
