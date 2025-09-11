7a37a8c1f6b513ca43477d0826d01cf7004c8dcb
Seonggyu-Min <minsunggu705@gmail.com>
2025-09-12 00:57:15 +0900

    feat: 미션 조회용 UI 제작 중

---
+++ Assets/00_WorkSpace/MSG/Scripts/Out Game UI/MissionUIBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace MSG
+{
+    public class MissionUIBehaviour : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _missionNameText;         // 미션 이름
+        [SerializeField] private TMP_Text _countText;               // 진행 상황 ( 예시 - 350/500 )
+        [SerializeField] private Image _incompletionProgressBar;    // 미완료 상태일 때의 게이지 바 (붉은색)
+        [SerializeField] private Image _completionProgress;         // 완료 상태일 때의 게이지 바 (초록색)
+        [SerializeField] private Image _goldIcon;                   // 보상 재화 종류 중 골드의 아이콘
+        [SerializeField] private Image _blueHoneyGemIcon;           // 보상 재화 종류 중 블루허니잼의 아이콘
+
+
+
+        public void Init()
+        {
+
+        }
+
+
+        public void OnClickClaimReward()
+        {
+
+        }
+    }
+}
