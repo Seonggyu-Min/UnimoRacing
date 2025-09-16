0d0b8be9f905d21d46aa45de9156079db8297f6a
Seonggyu-Min <minsunggu705@gmail.com>
2025-09-17 01:53:13 +0900

    feat: 프레임 디버깅 기능 추가

---
+++ Assets/00_WorkSpace/MSG/Scripts/Debugger/FrameDebugger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class FrameDebugger : MonoBehaviour
+    {
+        private float deltaTime = 0f;
+
+        [SerializeField] private int size = 25;
+        [SerializeField][Min(0f)] private float xPos = 30f;
+        [SerializeField][Min(0f)] private float yPos = 30f;
+        [SerializeField] private Color color = Color.red;
+
+        void Update()
+        {
+            deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        }
+
+        private void OnGUI()
+        {
+            GUIStyle style = new GUIStyle();
+
+            Rect rect = new Rect(xPos, yPos, Screen.width, Screen.height);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = size;
+            style.normal.textColor = color;
+
+            float ms = deltaTime * 1000f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.} FPS ({1:0.0} ms)", fps, ms);
+
+            GUI.Label(rect, text, style);
+        }
+    }
+}
