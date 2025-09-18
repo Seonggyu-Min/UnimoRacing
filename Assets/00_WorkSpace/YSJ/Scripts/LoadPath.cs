81c97d91f91e6827f27dff8ab92530e2a894c6f1
DevIRU <deviru0413@gmail.com>
2025-09-18 17:33:39 +0900

    [fix]: 아이템 로드 경로 추가

--- Assets/00_WorkSpace/YSJ/Scripts/LoadPath.cs
+++ Assets/00_WorkSpace/YSJ/Scripts/LoadPath.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class LoadPath
+﻿public static class LoadPath
 {
     // Resources
     public const string PLAYER_BASE_PREFAB_PATH     = "PlayerRacer";
+
+    public const string PLAYER_UNIMO_ITEM_PATH        = "SO/Unimo/Item/";
 
     public const string PLAYER_UNIMO_CHARACTER_SO   = "SO/Unimo/Character/UnimoCharacterSO";
     public const string PLAYER_UNIMO_KART_SO        = "SO/Unimo/Kart/UnimoKartSO";
