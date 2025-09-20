c2748cd4fb66272a9703ce51eea2fc91146a854a
Seonggyu-Min <minsunggu705@gmail.com>
2025-09-21 05:05:42 +0900

    chore: 로딩 시 사용할 유니모 DB 디버깅용 로그 추가

--- Assets/00_WorkSpace/MSG/Scripts/In Game UI/LoadingPlayerUIItem.cs
+++ Assets/00_WorkSpace/MSG/Scripts/In Game UI/LoadingPlayerUIItem.cs
@@ -22,9 +22,16 @@ namespace MSG
             }
             else
             {
+                Debug.Log("[LoadingPlayerUIItem] 유니모 스프라이트 등록 시도");
+
                 if (UnimoKartDatabase.Instance.TryGetByUnimoIndex(unimoIndex, out UnimoCharacterSO so))
                 {
                     _unimoImage.sprite = so.characterSprite;
+                    Debug.Log($"[LoadingPlayerUIItem] {so.characterSprite.name} 이 스프라이트로 등록됩니다.");
+                }
+                else
+                {
+                    Debug.Log($"[LoadingPlayerUIItem] {unimoIndex}번 유니모를 찾을 수 없습니다.");
                 }
             }
         }
