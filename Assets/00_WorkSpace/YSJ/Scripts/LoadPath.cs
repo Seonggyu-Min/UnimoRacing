197ca8e209ca63e3a23d7680957d07b8e769800d
DevIRU <deviru0413@gmail.com>
2025-09-21 00:04:01 +0900

    [feat]: 로프 경로 추가

--- Assets/00_WorkSpace/YSJ/Scripts/LoadPath.cs
+++ Assets/00_WorkSpace/YSJ/Scripts/LoadPath.cs
@@ -4,6 +4,8 @@
     public const string PLAYER_BASE_PREFAB_PATH     = "PlayerRacer";
 
     public const string PLAYER_UNIMO_ITEM_PATH        = "SO/Unimo/Item/";
+    public const string PLAYER_UNIMO_CHARACTER_PATH   = "SO/Unimo/Character/";
+    public const string PLAYER_UNIMO_KART_PATH        = "SO/Unimo/Kart/";
 
     public const string PLAYER_UNIMO_CHARACTER_SO   = "SO/Unimo/Character/UnimoCharacterSO";
     public const string PLAYER_UNIMO_KART_SO        = "SO/Unimo/Kart/UnimoKartSO";
