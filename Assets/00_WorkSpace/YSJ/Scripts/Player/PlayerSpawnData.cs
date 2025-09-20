0656190fb736e155ef938af3a2d0279c6d5196a2
DevIRU <deviru0413@gmail.com>
2025-09-21 00:07:47 +0900

    [feat]: 플레이어 스폰 데이터 구조 변경

--- Assets/00_WorkSpace/YSJ/Scripts/Player/PlayerSpawnData.cs
+++ Assets/00_WorkSpace/YSJ/Scripts/Player/PlayerSpawnData.cs
@@ -6,30 +6,4 @@ public class PlayerSpawnData
 {
     public int CharacterID = -1;
     public int KartID = -1;
-
-    [Header("Default Inject SO")]
-    [SerializeField] private UnimoCharacterSO injectDefaultCharacterSO;
-    [SerializeField] private UnimoKartSO injectDefaultInJectKartSO;
-
-    public UnimoCharacterSO InjectCharacterSO
-    {
-        get
-        {
-            if (injectDefaultCharacterSO == null)
-                injectDefaultCharacterSO = Resources.Load<UnimoCharacterSO>($"{LoadPath.PLAYER_UNIMO_CHARACTER_SO}_{0}");
-
-            return injectDefaultCharacterSO;
-        }
-    }
-
-    public UnimoKartSO InjectKartSO
-    {
-        get
-        {
-            if (injectDefaultInJectKartSO == null)
-                injectDefaultInJectKartSO = Resources.Load<UnimoKartSO>($"{LoadPath.PLAYER_UNIMO_KART_SO}_{0}");
-
-            return injectDefaultInJectKartSO;
-        }
-    }
 }
