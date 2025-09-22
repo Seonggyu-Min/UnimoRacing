6939523b397e53106b3796d179e906a967a2dbea
DevIRU <deviru0413@gmail.com>
2025-09-22 14:48:02 +0900

    [fix]: 스폰된 Player의 레이싱 관련 데이터, InGameManager 쪽의 호출 타이밍 액션 파트 수정
    OnRaceState_LoadPlayers > OnRaceState_Countdown = OnPlayReady

--- Assets/00_WorkSpace/YSJ/Scripts/Player/PlayerRaceData.cs
+++ Assets/00_WorkSpace/YSJ/Scripts/Player/PlayerRaceData.cs
@@ -41,9 +41,6 @@ public class PlayerRaceData : MonoBehaviour, IPunInstantiateMagicCallback
     [SerializeField] private bool _isMovable = false;        // 이동 가능 여부
     [SerializeField] private bool _isItemUsable = false;     // 아이템 사용가능 여부
 
-    public Action OnBeforeRaceSetupAction;
-    public Action OnAfterRaceSetupAction;
-
     private bool _isEndRace = false;
     private int _currentTrackIndex = -1;
     private int _lap = 0;
@@ -260,9 +257,9 @@ public class PlayerRaceData : MonoBehaviour, IPunInstantiateMagicCallback
         this.PrintLog("GameManagerSetup 진행");
         if (_inGM != null && _useGM)
         {
-            // 로드
-            _inGM.OnRaceState_LoadPlayers -= OnPlayReady;
-            _inGM.OnRaceState_LoadPlayers += OnPlayReady;
+            // 카운트 다운
+            _inGM.OnRaceState_Countdown -= OnPlayReady;
+            _inGM.OnRaceState_Countdown += OnPlayReady;
 
             // 레이싱
             _inGM.OnRaceState_Racing -= OnPlayRaceEnter;
@@ -296,8 +293,6 @@ public class PlayerRaceData : MonoBehaviour, IPunInstantiateMagicCallback
         {
             if (_inGM != null && _lap >= _inGM.RaceEndLapCount)
             {
-                PhotonNetworkCustomProperties.LocalPlayerRaceFinishedSetting(PhotonNetwork.Time);
-
                 _isControlable = false;
                 _isMovable = false;
                 _isItemUsable = false;
@@ -544,7 +539,6 @@ public class PlayerRaceData : MonoBehaviour, IPunInstantiateMagicCallback
             $"_isSetups: {_isSetups}\n" +
             $"");
 
-        OnBeforeRaceSetupAction?.Invoke();
 
         // Setup(순서: (Kart > Character) > (Controller > Movement) > Sync > (Cam > AniCtrl > Synergy))
         // Visual
@@ -594,9 +588,9 @@ public class PlayerRaceData : MonoBehaviour, IPunInstantiateMagicCallback
 
         // 플레이어의 커스텀 프롬퍼티 생성 시점 > 매칭이 되었을 때
         // 룸데이터는 그 이전에 되어 있어야된다.
-        
+        var pm = PlayerManager.Instance;
+        pm?.SetPlayerCPRaceLoaded(IsSetups);
 
-        OnAfterRaceSetupAction?.Invoke();
         this.PrintLog("Delay Load Data 진행 완료");
     }
 
