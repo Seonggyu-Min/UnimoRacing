969fe7febd5b35ba48ca66b8972a9d1f080a4770
DevIRU <deviru0413@gmail.com>
2025-09-23 14:48:10 +0900

    [test]: 테스트용 아이템 인벤토리 기능 세부설정 가능하게 추가

--- Assets/00_WorkSpace/YSJ/Scripts/Player/ItemInventory.cs
+++ Assets/00_WorkSpace/YSJ/Scripts/Player/ItemInventory.cs
@@ -16,9 +16,12 @@ public class ItemInventory : MonoBehaviour
     [SerializeField] private int _inventroySaveMaxCount = 2;
 
     // publilc Action
+    public Action<UnimoItemSO> OnSaveItem;
+    public Action<UnimoItemSO> OnRemoveItem;
 
     // private Parameter
     private bool _isSetup = false;
+    private bool _isSavable = false;
 
     private PhotonView _ownerView;
     private PlayerRaceData _data;
@@ -55,19 +58,33 @@ public class ItemInventory : MonoBehaviour
     }
 
     #region Save
-    public void SaveItem(UnimoItemSO itemSO, bool isDelaySave = false)
+    public void SaveItem(UnimoItemSO itemSO, bool isDelaySave = false, bool isForceSave = false, bool isSaveItemAction = true)
     {
-        if (!_isSetup)
-        {
-            this.PrintLog("셋업이 정상적으로 진행되지 않았습니다.");
-            return;
-        }
-
         if (itemSO == null)
         {
             this.PrintLog("해당 아이템의 SO 데이터가 비어 있어서 인벤토리에 정상적인 저장을 할 수 없습니다.");
             return;
         }
+
+        string itmeNameString = $"{itemSO.name}_{ itemSO.itemID}";
+        if (!_isSetup)
+        {
+            this.PrintLog($"셋업이 정상적으로 진행되지 않았습니다. [저장 시도 아이템 {itmeNameString}]");
+            return;
+        }
+
+        if (!_isSavable && !isForceSave)
+        {
+            this.PrintLog($"저장 가능한 상태가 아닙니다. 저장할 수 없습니다. [저장 시도 아이템 {itmeNameString}]");
+            return;
+        }
+
+        if (isDelaySave)
+            this.PrintLog($"지연 저장을 시도 할 수 있는 상태입니다. [저장 시도 아이템 {itmeNameString}]");
+
+        if (isForceSave)
+            this.PrintLog($"강제 저장을 시도 할 수 있는 상태입니다. [저장 시도 아이템 {itmeNameString}]");
+
 
         int nullIndex = -1;
         // 비어있는 인덱스 체크
@@ -90,12 +107,19 @@ public class ItemInventory : MonoBehaviour
                 this.PrintLog(" 지연저장 가능 아이템을 지연 저장 파트에 넣어 처리합니다.");
             }
             else
-                this.PrintLog($"인벤토리가 다 차있어 저장을 못합니다.[저장 시도 아이템 {itemSO.name}_{itemSO.itemID}]");
+                this.PrintLog($"인벤토리가 다 차있어 저장을 못합니다. [저장 시도 아이템 {itmeNameString}]");
             return;
         }
 
-        _items[nullIndex] = itemSO;
-        this.PrintLog($"아이템을 저장합니다.[저장 인덱스: {nullIndex} / 저장 아이템 {itemSO.name}_{itemSO.itemID}]");
+        if (itemSO.isInventorySavable)
+        {
+            _items[nullIndex] = itemSO;
+        }
+
+        if (isSaveItemAction)
+            OnSaveItem?.Invoke(itemSO);
+
+        this.PrintLog($"아이템을 저장합니다.[저장 인덱스: {nullIndex} / 저장 아이템 {itmeNameString}]");
     }
     private void DelaySaveItem(int saveIndex)
     {
@@ -143,27 +167,27 @@ public class ItemInventory : MonoBehaviour
     /// 보유 아이템, 인텍스로 제거
     /// </summary>
     /// <param name="index">제거 하고 싶은 보유 배열 인덱스</param>
-    public void RemoveItemForIndex(int index)
+    public void RemoveItemForIndex(int index, bool delaySavable = true)
     {
-        UnimoItemSO resultItem = FindItemForIndex(index);
-        RemoveItemForSO(resultItem);
+        UnimoItemSO resultItem = FindItemByIndex(index);
+        RemoveItemBySO(resultItem, delaySavable);
     }
-    
+
     /// <summary>
     /// 보유 아이템, 아이디로 제거
     /// </summary>
     /// <param name="id">제거 하고 싶은 아이템 ID</param>
-    public void RemoveItemForID(int id)
+    public void RemoveItemByID(int id, bool delaySavable = true)
     {
-        UnimoItemSO resultItem = FindItemForID(id);
-        RemoveItemForSO(resultItem);
+        UnimoItemSO resultItem = FindItemByID(id);
+        RemoveItemBySO(resultItem, delaySavable);
     }
 
     /// <summary>
     /// 보유 아이템, SO로 제거
     /// </summary>
     /// <param name="inItemSO">제거 하고 싶은 아이템</param>
-    public void RemoveItemForSO(UnimoItemSO inItemSO)
+    public void RemoveItemBySO(UnimoItemSO inItemSO, bool delaySavable = true)
     {
         if (!_isSetup)
         {
@@ -196,16 +220,19 @@ public class ItemInventory : MonoBehaviour
 
         var removeItem = _items[removeIndex];
         _items[removeIndex] = null;
+        OnRemoveItem?.Invoke(removeItem);
+
         this.PrintLog($"인벤토리 제거 대상 아이템(=> {removeItem.itemName}) > 인벤토리에서 제거(=> 제거 여부: {_items[removeIndex] == null})");
 
         // 아이템 지연 저장 기능
-        DelaySaveItem(removeIndex);
+        if (delaySavable)
+            DelaySaveItem(removeIndex);
     }
 
     #endregion
 
     #region Find
-    public UnimoItemSO FindItemForIndex(int index)
+    public UnimoItemSO FindItemByIndex(int index)
     {
         if (!_isSetup)
         {
@@ -221,7 +248,7 @@ public class ItemInventory : MonoBehaviour
 
         return _items[index];
     }
-    public UnimoItemSO FindItemForID(int id)
+    public UnimoItemSO FindItemByID(int id)
     {
         if (!_isSetup)
         {
@@ -246,10 +273,10 @@ public class ItemInventory : MonoBehaviour
         }
         return resultItem;
     }
-    public int FindItemIndexForSO(UnimoItemSO itemSO)
+    public int FindItemIndexBySO(UnimoItemSO itemSO)
     {
         int result = -1;
-        if(itemSO == null)
+        if (itemSO == null)
         {
             this.PrintLog($"`아이템 SO` 데이터가 NULL이여서 SO로 ItemIndex 찾기를 중단합니다.");
             return result;
@@ -258,7 +285,7 @@ public class ItemInventory : MonoBehaviour
         for (int i = 0; i < _items.Length; i++)
         {
             var item = _items[i];
-            if (item == null) 
+            if (item == null)
                 continue;
             ItemId checkItemSOID = _items[i].itemID;
             if (itemSO.itemID.Equals(checkItemSOID))
@@ -271,11 +298,28 @@ public class ItemInventory : MonoBehaviour
     }
     #endregion
 
-    #region Contains
+    #region Util
     public bool ItemContains(UnimoItemSO itemSO)
     {
-        int id = FindItemIndexForSO(itemSO);
+        int id = FindItemIndexBySO(itemSO);
         return (id != -1 && id >= 0);
+    }
+
+    public int ItemCount()
+    {
+        int result = 0;
+        for (int i = 0; i < _items.Length; i++)
+        {
+            if (FindItemByIndex(i) != null)
+                result++;
+        }
+
+        return result;
+    }
+
+    public void SetSavable(bool isSavable)
+    {
+        _isSavable = isSavable;
     }
     #endregion
 
