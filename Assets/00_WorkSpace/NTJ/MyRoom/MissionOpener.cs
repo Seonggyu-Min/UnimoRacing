7348d180b1a0ee915b5da8594699195db11e2f8c
Seonggyu-Min <minsunggu705@gmail.com>
2025-09-23 12:58:10 +0900

    refactor: Close 버튼 이관

---
+++ Assets/00_WorkSpace/NTJ/MyRoom/MissionOpener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class MissionOpener : MonoBehaviour
+    {
+        public void OnClickShowMissionButton()
+        {
+            UIManager.Instance.Show("Mission Panel");
+        }
+
+        public void OnClickHideMissionButton()
+        {
+            UIManager.Instance.Hide("Mission Panel");
+        }
+    }
+}
