746a256a6a57cdb2c1e6bbdf1807a06904523641
Seonggyu-Min <minsunggu705@gmail.com>
2025-09-25 17:30:35 +0900

    feat: 랜덤 박스를 맵 인덱스별로 활성화 하는 기능 추가

---
+++ Assets/00_WorkSpace/MSG/Scripts/Test/RandomBoxEnabler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSG
+{
+    // 테스트용 코드이며 실제 배포 시 사용하면 안될 것 같은 구조입니다.
+    public class RandomBoxEnabler : MonoBehaviour
+    {
+        [SerializeField] private GameObject _map1Obj;
+        [SerializeField] private GameObject _map2Obj;
+        [SerializeField] private GameObject _map3Obj;
+
+        private void Start()
+        {
+            int index = PhotonNetworkCustomProperties.GetRoomProp<int>(RoomKey.WinnerMapIndex);
+
+            if (index == 1)
+            {
+                _map1Obj?.SetActive(true);
+                _map2Obj?.SetActive(false);
+                _map3Obj?.SetActive(false);
+            }
+            else if (index == 2)
+            {
+                _map1Obj?.SetActive(false);
+                _map2Obj?.SetActive(true);
+                _map3Obj?.SetActive(false);
+            }
+            else if (index == 3)
+            {
+                _map1Obj?.SetActive(false);
+                _map2Obj?.SetActive(false);
+                _map3Obj?.SetActive(true);
+            }
+            else
+            {
+                Debug.LogWarning($"[RandomBoxEnabler] 투표된 맵 인덱스: {index}의 값이 비정상적입니다.");
+            }
+        }
+    }
+}
