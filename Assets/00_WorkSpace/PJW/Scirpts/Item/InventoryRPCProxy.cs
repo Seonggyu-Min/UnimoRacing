a9bfee408d7f9db9d09b2bc692f575efd9a3c0a5
DevIRU <deviru0413@gmail.com>
2025-09-28 23:08:00 +0900

    [feat]: RPC 데이터 받는 중계 기능 추가

---
+++ Assets/00_WorkSpace/PJW/Scirpts/Item/InventoryRPCProxy.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using YSJ;
+using YSJ.Util;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(PhotonView))]
+public class InventoryRPCProxy : MonoBehaviourPun
+{
+    [PunRPC]
+    public void RPC_GrantItemById(int itemIdInt, bool isDelaySave = false, bool isForceSave = false, bool isSaveItemAction = true)
+    {
+        this.PrintLog("ItemInventory에 Set 시도");
+        var inv = GetComponentInChildren<ItemInventory>(true);
+        if (inv == null)
+        {
+            this.PrintLog("ItemInventory 미발견", LogType.Warning);
+            return;
+        }
+
+        var so = ItemManager.Instance.ResolveById((ItemId)itemIdInt);
+        if (so == null || so.itemID == ItemId.None)
+        {
+            this.PrintLog($"Resolve 실패 or None: {(ItemId)itemIdInt}", LogType.Warning);
+            return;
+        }
+
+        this.PrintLog("ItemInventory에 Set 처리 시도");
+        inv.SaveItem(so, isDelaySave, isForceSave, isSaveItemAction);
+    }
+}
