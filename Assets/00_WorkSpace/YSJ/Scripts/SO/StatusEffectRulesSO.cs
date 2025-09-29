3dbccbaf0f9433f37fa5988ae0bac4d5acdc274c
DevIRU <deviru0413@gmail.com>
2025-09-29 16:15:35 +0900

    [feat]: StatusEffectRulesSO 클래스 추가, 여러 상태를 관리를 담당 SO 추가

---
+++ Assets/00_WorkSpace/YSJ/Scripts/SO/StatusEffectRulesSO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewStatusEffectRulesSO", menuName = "Game/StatusEffectRules")]
+public class StatusEffectRulesSO : ScriptableObject
+{
+    public List<StatusEffectRuleData> rules = new();
+    public StatusEffectRuleData Get(StatusEffect e) => rules.Find(r => r.effect == e);
+
+    private void OnValidate()
+    {
+        foreach (var r in rules)
+        {
+            if (r == null || r.exclusiveWith == null)
+                continue;
+
+            int findIndex = r.exclusiveWith.IndexOf(r.effect);
+            if (findIndex != -1)
+                r.exclusiveWith.RemoveAt(findIndex);
+        }
+    }
+}
