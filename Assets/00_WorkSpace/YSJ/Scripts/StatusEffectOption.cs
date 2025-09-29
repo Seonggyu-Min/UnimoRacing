6f624e18d91057eb798d277b47b9fecff5e8beff
DevIRU <deviru0413@gmail.com>
2025-09-29 23:14:06 +0900

    [feat]: StatusEffectOption에 있던 StatusEffect를 ProjectEnum 이관

--- Assets/00_WorkSpace/YSJ/Scripts/StatusEffectOption.cs
+++ Assets/00_WorkSpace/YSJ/Scripts/StatusEffectOption.cs
@@ -1,19 +1,9 @@
 ﻿using System;
 
-public enum StatusEffect
-{
-    Slow,           // 이동속도 감소
-    Haste,          // 이동속도 가속
-    Slip,           // 미끄러짐
-    Silence,        // 침묵
-    Immunity,       // 면역
-    Airborne,       // 에어본
-    Root,           // 속박
-    Blind           // 시야 차단
-}
+
 
 [Serializable]
-public class StatusEffectOption
+public struct StatusEffectOption
 {
     // 적용 시킬 효과
     public StatusEffect optionStatusEffect;
