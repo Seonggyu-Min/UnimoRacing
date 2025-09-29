c0a6afd5ef7bd0a8a18cdafa6cb34d72f5b34e39
DevIRU <deviru0413@gmail.com>
2025-09-29 23:33:25 +0900

    [feat]: StatusEffectOption에 있던 ItemId를 ProjectEnum 이관

--- Assets/00_WorkSpace/YSJ/Scripts/SO/UnimoItemSO.cs
+++ Assets/00_WorkSpace/YSJ/Scripts/SO/UnimoItemSO.cs
@@ -1,18 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemId
-{
-    None = 0,
-    Booster = 30001,    // 부스터
-    ThrowBomb,          // 투척용 폭탄 (Thrown/Throwable Bomb 계열)
-    Padlock,            // 자물쇠 (락/구속 트랩)
-    Shield,             // 실드
-    SmokeScreen,        // 시야차단 (연막/잉크 등)
-    Missile,            // 미사일 (유도/직선형 모두 커버)
-    FriedEgg            // 계란후라이 (바나나 대체 트랩 느낌)
-}
-
 [CreateAssetMenu(fileName = "NewUnimoItemSO", menuName = "Unimo/Item")]
 public class UnimoItemSO : ScriptableObject
 {
@@ -26,19 +14,20 @@ public class UnimoItemSO : ScriptableObject
     [Tooltip("아이템 아이콘 이미지")]
     public Sprite itemIconSprite;
 
-    [Tooltip("아이템 효과 최대 스택 수")]
+    [Tooltip("아이템 저장 가능 상태 여부")]
     public bool isInventorySavable = false;
 
-    [Tooltip("아이템 지속 시간")]
-    public float itemEffectDuration;
+    // =====================================================================
 
     [Header("효과 관련 설정")]
-    [Tooltip("아이템 효과 적용 시, 효과 초기화(false 시, 해당 아이템 상태가 완전히 끝났을 때 적용됩니다. 스택 수가 무시됩니다.)")]
-    public bool isReapplication = false;
+    [Tooltip("효과 재적용 모드")]
+    public ReapplyMode reapplyMode = ReapplyMode.RefreshDuration;
 
-    [Tooltip("아이템 효과 최대 스택 수")]
+    [Tooltip("효과 지속 시간")]
+    public float itemEffectDuration;
+
+    [Tooltip("효과 최대 스택 수"), Min(1)]
     public float stackCount = 1;
-
 
     public List<StatusEffectOption> options;
 }
