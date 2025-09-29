abe185d79cff708042513c70097ffdbbce454e6c
DevIRU <deviru0413@gmail.com>
2025-09-29 16:10:58 +0900

    [feat]: StatusEffectRuleData 클래스 추가, 상태 효과에 대한 룰 설정 기능 추가

---
+++ Assets/00_WorkSpace/YSJ/Scripts/SO/StatusEffectRuleData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+/// <summary>
+/// 상태 효과 룰
+/// </summary>
+[Serializable]
+public class StatusEffectRuleData
+{
+    /// <summary>
+    /// 규칙이 적용되는 주 대상 효과
+    /// </summary>
+    public StatusEffect effect;
+
+    [Tooltip("서로 공존할 수 없는 다른 효과")]
+    public List<StatusEffect> exclusiveWith = new();
+    [Tooltip("효과 적용 시, 더 강한 쪽을 남길지 여부(true=강한 쪽, false=마지막 적용)")]
+    public bool preferStronger = true;
+}
