1cd399f2c7b504cb835f2ac0238b1ecfa9846ee3
Seonggyu-Min <minsunggu705@gmail.com>
2025-10-13 17:09:36 +0900

    feat: 친구 신청 input field 초기화 기능 추가

---
+++ Assets/00_WorkSpace/MSG/Scripts/Out Game UI/InputFieldResetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+namespace MSG
+{
+    public class InputFieldResetter : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField _inputField;
+
+        private void OnDisable()
+        {
+            ResetInputField();
+        }
+
+        public void ResetInputField()
+        {
+            _inputField.text = string.Empty;
+        }
+    }
+}
