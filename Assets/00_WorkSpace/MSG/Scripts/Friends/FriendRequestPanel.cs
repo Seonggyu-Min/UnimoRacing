c55ad92e014dbedcf061ec5044f12f9aa703f10d
ro9ro9 <101381135+ro9ro9@users.noreply.github.com>
2025-10-15 17:34:35 +0900

    [feat]친구 추가 닉네임 나오게 변경

--- Assets/00_WorkSpace/MSG/Scripts/Friends/FriendRequestPanel.cs
+++ Assets/00_WorkSpace/MSG/Scripts/Friends/FriendRequestPanel.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 namespace MSG
 {
     public class FriendRequestPanel : MonoBehaviour
     {
-       // [SerializeField] private TMP_Text _infoText;
+        [SerializeField] private TMP_Text _nicknameText;
+        [SerializeField] private TMP_Text _levelText;
+        [SerializeField] private Image _unimoIcon;
+
         private string _pairId;
         private string _toUid;
         private string _fromUid;
@@ -19,23 +21,58 @@ namespace MSG
             _toUid = toUid;
             _fromUid = fromUid;
 
-            // 닉네임 비동기 로드라서 미리 텍스트 설정
-            //_infoText.text = "요청 로딩 중…";
+            LoadFriendInfo(_fromUid);
+        }
 
-            DatabaseManager.Instance.GetOnMain(DBRoutes.Nickname(_fromUid),
-               // snap => _infoText.text = $"{snap.Value}",
-                err => Debug.LogWarning($"친구 요청 로딩 에러: {err}")
+        private void LoadFriendInfo(string uid)
+        {
+            DatabaseManager.Instance.GetOnMain(
+                DBRoutes.Users(uid),
+                snap =>
+                {
+                    // 닉네임
+                    string nickname = "";
+                    var nickSnap = snap.Child(DatabaseKeys.nickname);
+                    if (nickSnap.Exists && nickSnap.Value != null)
+                        nickname = nickSnap.Value.ToString();
+
+                    _nicknameText.text = nickname;
+
+                    // 레벨
+                    int level = 1;
+                    var expSnap = snap.Child(DatabaseKeys.gameData).Child(DatabaseKeys.experience);
+                    if (expSnap.Exists && expSnap.Value != null)
+                    {
+                        long exp = 0;
+                        long.TryParse(expSnap.Value.ToString(), out exp);
+                        level = ExpToLevel.LevelFromTotalExp((int)exp);
+                    }
+                    _levelText.text = $"lv {level}";
+
+                    // 유니모 아이콘
+                    int equippedIndex = -1;
+                    var unimoSnap = snap.Child(DatabaseKeys.equipped).Child(DatabaseKeys.unimos);
+                    if (unimoSnap.Exists && unimoSnap.Value != null)
+                        int.TryParse(unimoSnap.Value.ToString(), out equippedIndex);
+
+                    if (UnimoKartDatabase.Instance.TryGetByUnimoIndex(equippedIndex, out UnimoCharacterSO unimo) &&
+                        unimo != null && unimo.characterSprite != null)
+                    {
+                        _unimoIcon.sprite = unimo.characterSprite;
+                    }
+                },
+                err => Debug.LogWarning($"[FriendRequestPanel] 유저 정보 로드 실패: {err}")
             );
         }
 
-        public void OnClickAccecpt()
+        public void OnClickAccept()
         {
             FriendsLogics.Instance.AcceptRequest(
                 _pairId,
                 _toUid,
                 () => Debug.Log("친구 수락 성공"),
                 err => Debug.LogWarning($"{err}")
-                );
+            );
         }
 
         public void OnClickReject()
@@ -45,7 +82,7 @@ namespace MSG
                 _toUid,
                 () => Debug.Log("친구 거절 성공"),
                 err => Debug.LogWarning($"{err}")
-                );
+            );
         }
     }
 }
